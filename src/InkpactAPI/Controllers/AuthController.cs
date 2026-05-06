937285d02f5ab0f0aa97f82733aff6df83e25df9
Elvis Nilsson <Nilssonelvis@gmail.com>
2026-05-06 12:56:42 +0200

    Done with 2 ext and auth controller

---
+++ src/InkpactAPI/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Application.Auth.Commands.Login;
+using Application.Auth.Commands.Register;
+using InkpactAPI.Common;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace InkpactAPI.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result.ToActionResult();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result.ToActionResult();
+        }
+    }
+}
