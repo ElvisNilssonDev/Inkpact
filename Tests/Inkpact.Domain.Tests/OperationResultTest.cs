bb0ab46e80b48f7efe8c140ccca13131a4f86101
Elvis Nilsson <Nilssonelvis@gmail.com>
2026-05-18 16:43:04 +0200

    The First unittest is done

--- Tests/Inkpact.Domain.Tests/OperationResultTest.cs
+++ Tests/Inkpact.Domain.Tests/OperationResultTest.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Common;
+using FluentAssertions;
+using Xunit;
 
-namespace Tests.Inkpact.Domain.Tests
+namespace Inkpact.Domain.Tests.Common;
+
+public class OperationResultTests
 {
-    internal class OperationResultTest
+    [Fact]
+    public void Success_WithData_ReturnsIsSuccessTrueAndData()
     {
+        var guid = Guid.NewGuid();
+
+        var result = OperationResult<Guid>.Success(guid);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Data.Should().Be(guid);
+        result.Status.Should().Be(OperationResultStatus.Ok);
+    }
+
+    [Fact]
+    public void Failure_WithMessage_ReturnsIsSuccessFalseAndCorrectStatus()
+    {
+        var result = OperationResult<Guid>.Failure("Not found", OperationResultStatus.NotFound);
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Be("Not found");
+        result.Status.Should().Be(OperationResultStatus.NotFound);
     }
 }
